bb6b731373298aed0bf7743244c3381b353b2e52
Jose Lima <jalimag0206@gmail.com>
2024-03-31 00:36:20 -0300

    refactor: remove create user from user resource

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PasswordHasherAdapter } from 'src/adapters/password-hasher';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PasswordHasherAdapter],
+  providers: [UsersService],
 })
 export class UsersModule {}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,32 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Injectable } from '@nestjs/common';
 
 import { PrismaUsersRepository } from 'src/repositories/prisma/users.repository';
-import { PasswordHasherAdapter } from 'src/adapters/password-hasher';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    private readonly usersRepository: PrismaUsersRepository,
-    private readonly passwordHasher: PasswordHasherAdapter,
-  ) {}
-
-  async create(createUserDto: CreateUserDto) {
-    const { name, email, password } = createUserDto;
-
-    const emailAlreadyInUse = await this.usersRepository.findByEmail(email);
-
-    const hashedPassword = await this.passwordHasher.hash(password);
-    if (emailAlreadyInUse) {
-      throw new ConflictException('This email is already in use');
-    }
-
-    const user = await this.usersRepository.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    return user;
-  }
+  constructor(private readonly usersRepository: PrismaUsersRepository) {}
 }
