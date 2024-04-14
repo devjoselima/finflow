8bf53b2d9964034b77c95addda80bcce3ba084c5
Jose Lima <jalimag0206@gmail.com>
2024-04-14 15:22:33 -0300

    feat: add users resource

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,4 +5,8 @@ import { PrismaUsersRepository } from 'src/repositories/prisma/users.repository'
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: PrismaUsersRepository) {}
+
+  getUserById(userId: string) {
+    return { userId };
+  }
 }
